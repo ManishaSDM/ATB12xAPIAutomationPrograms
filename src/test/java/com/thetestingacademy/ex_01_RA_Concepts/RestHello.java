864fed8eb30551e0cc18bf276f7884c514713187
ManishaSDM <manisha.shindeistqbf@gmail.com>
2025-08-06 23:48:19 -0500

    REST API automation project using Rest Assured in Java,Assertion and pojo added

--- src/test/java/com/thetestingacademy/ex_01_RA_Concepts/RestHello.java
+++ src/test/java/com/thetestingacademy/ex_01_RA_Concepts/RestHello.java
@@ -1,4 +1,17 @@
 package com.thetestingacademy.ex_01_RA_Concepts;
 
+import io.restassured.RestAssured;
+
 public class RestHello {
+    public static void main(String[] args) {
+    //https://api.postalpincode.in/pincode/560036
+        //baseURI is https://api.postalpincode.in/
+        //basePath is pin code /560036
+        RestAssured.given()
+                .baseUri("https://api.postalpincode.in/pincode/560036")
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
 }
---
+++ src/test/java/com/thetestingacademy/ex_07_Payload_management/Class/Manualway/BookingDates.java
@@ -0,0 +1,22 @@
+package com.thetestingacademy.ex_07_Payload_management.Class.Manualway;
+
+public class BookingDates {
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    private String checkin;
+    private String checkout;
+}
